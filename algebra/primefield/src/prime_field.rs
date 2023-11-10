e7947dfa201a799963b1c1728aa7a443a7b9d8c3
sergisi <sergisiba@gmail.com>
2023-11-10 14:40:48 +0100

    ??

--- algebra/primefield/src/prime_field.rs
+++ algebra/primefield/src/prime_field.rs
@@ -32,7 +32,7 @@ use zkp_u256::{
 // Derive fails for Clone, PartialEq, Eq, Hash
 pub struct PrimeField<P: Parameters> {
     // TODO: un-pub. They are pub so FieldElement can have const-fn constructors.
-    pub uint:        P::UInt,
+    pub uint: P::UInt,
     pub _parameters: PhantomData<P>,
 }
 
